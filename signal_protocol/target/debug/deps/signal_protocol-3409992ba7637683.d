f40fdcb027298d9b55335fe5d60179dfefafed1e
Egor <svetlega10@gmail.com>
2025-03-24 10:49:54 +0300

    init Signal Protocol

---
+++ signal_protocol/target/debug/deps/signal_protocol-3409992ba7637683.d
@@ -0,0 +1,5 @@
+C:\Users\Honor\projects\Rust\signal_protocol\signal_protocol\target\debug\deps\libsignal_protocol-3409992ba7637683.rmeta: src\main.rs
+
+C:\Users\Honor\projects\Rust\signal_protocol\signal_protocol\target\debug\deps\signal_protocol-3409992ba7637683.d: src\main.rs
+
+src\main.rs:
---
+++ signal_protocol/target/debug/deps/signal_protocol-64bc4a26aaaf5315.d
@@ -0,0 +1,5 @@
+C:\Users\Honor\projects\Rust\signal_protocol\signal_protocol\target\debug\deps\libsignal_protocol-64bc4a26aaaf5315.rmeta: src\main.rs
+
+C:\Users\Honor\projects\Rust\signal_protocol\signal_protocol\target\debug\deps\signal_protocol-64bc4a26aaaf5315.d: src\main.rs
+
+src\main.rs:
